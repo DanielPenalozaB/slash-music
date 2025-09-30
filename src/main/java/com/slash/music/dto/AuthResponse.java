06300a45808587b7aa462a83cb117895552653ee
0xCamiX <juancamilogallego70@icloud.com>
2025-09-29 21:30:51 -0500

    refactor(auth): remove TokenType field from AuthResponse for cleaner code

--- src/main/java/com/slash/music/dto/AuthResponse.java
+++ src/main/java/com/slash/music/dto/AuthResponse.java
@@ -12,6 +12,5 @@ import lombok.NoArgsConstructor;
 public class AuthResponse {
   private String accessToken;
   private String refreshToken;
-  private final String TokenType = "Bearer";
   private UserResponseDTO user;
 }
